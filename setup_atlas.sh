4d36b1be9e39ccb75949ff1053fdc04523cdb3e7
LLEFEVRE <loic.lefevre@gmail.com>
2023-10-30 17:34:20 +0100

    Atlas integration

--- setup_atlas.sh
+++ setup_atlas.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 #systemctl --user start podman.socket
 
-export PASSWORD=My_Strong_Pa55word
-export HOST=db23c.micronautdata.vcnatlas.oraclevcn.com
-export SERVICE=pdb1.micronautdata.vcnatlas.oraclevcn.com
+echo "Configuring database connectivity for ${1}..."
 
-/home/opc/sqlcl/bin/sql -s system/$PASSWORD@$HOST:1521/$SERVICE <<EOF
-    create user micronaut_data_$RUNID identified by "Oracle_19_Password" DEFAULT TABLESPACE USERS TEMPORARY TABLESPACE TEMP;
-    alter user micronaut_data_$RUNID quota unlimited on users;
-    grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to micronaut_data_$RUNID;
-EOF
+export INFO=$(curl -s -k -L -X GET "https://api.atlas-controller.oraclecloud.com/ords/atlas/admin/database?type=${1}&hostname=`hostname`" -H 'accept: application/json')
+export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
+export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
+export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-export JDBC_URL="jdbc:oracle:thin:@${HOST}:1521/${SERVICE}"
-export JDBC_USER="micronaut_data_${RUNID}"
-export JDBC_PASSWORD="Oracle_19_Password"
+#export PASSWORD=My_Strong_Pa55word
+#export HOST=db23c.micronautdata.vcnatlas.oraclevcn.com
+#export SERVICE=pdb1.micronautdata.vcnatlas.oraclevcn.com
+
+case "${1}" in
+    "autonomous")
+      curl -s -X POST "https://${HOST}.oraclecloudapps.com/ords/admin/_/sql" -H 'content-type: application/sql' -H 'accept: application/json' -basic -u admin:${PASSWORD} --data-ascii "create user micronaut_data_$RUNID identified by \"Oracle_19_Password\" DEFAULT TABLESPACE DATA TEMPORARY TABLESPACE TEMP;alter user micronaut_data_$RUNID quota unlimited on data;grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to micronaut_data_$RUNID;"
+
+      export JDBC_URL="jdbc:oracle:thin:@(description=(retry_count=5)(retry_delay=1)(address=(protocol=tcps)(port=1521)(host=${HOST}.oraclecloud.com))(connect_data=(service_name=${SERVICE}_tp.adb.oraclecloud.com))(security=(ssl_server_dn_match=no)))?oracle.jdbc.enableQueryResultCache=false&oracle.jdbc.thinForceDNSLoadBalancing=true&tcp.nodelay=yes"
+      export JDBC_USER="micronaut_data_${RUNID}"
+      export JDBC_PASSWORD="Oracle_19_Password"
+      ;;
+    *)
+      /home/opc/sqlcl/bin/sql -s system/$PASSWORD@$HOST:1521/$SERVICE <<EOF
+          create user micronaut_data_$RUNID identified by "Oracle_19_Password" DEFAULT TABLESPACE USERS TEMPORARY TABLESPACE TEMP;
+          alter user micronaut_data_$RUNID quota unlimited on users;
+          grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to micronaut_data_$RUNID;
+      EOF
+
+      export JDBC_URL="jdbc:oracle:thin:@${HOST}:1521/${SERVICE}"
+      export JDBC_USER="micronaut_data_${RUNID}"
+      export JDBC_PASSWORD="Oracle_19_Password"
+      ;;
+esac;
+
 
 cat <<EOF > ./data-jdbc/src/test/resources/application.yml
 test-resources:
